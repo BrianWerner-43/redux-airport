cb2266cf880e92177f38ec9e111d1464e84ca5d6
Brian Werner <brianmw199@gmail.com>
2023-11-28 14:53:52 -0600

    Created our form with some airlines

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,39 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
 
 function App() {
+  let [planeInput, setPlaneInput] = useState('')
+  const ourAirlines = useSelector((store) => store.airlines)
+
+
+  const dispatch = useDispatch();
+
+  const addAirline = (e) => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'ADD_AIRPLANE',
+      payload: planeInput
+    })
+  }
+
+
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
+      <form onSubmit={addAirline}>
+      <input placeholder='Airline Name'
+      value={planeInput}
+      onChange={(e) => setPlaneInput(e.target.value)}/>
       <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      </form>
+      <table>{ourAirlines.map((index) => {
+        return <tr>{index}</tr>
+      })}</table>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,33 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
 /** TODO: import REDUX **/
-
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 /** TODO: Add REDUCERS */
-
+const airlines = (state =[], action) => {
+    if(action.type === 'ADD_AIRPLANE') {
+        const newplane = action.payload
+        console.log(newplane)
+        return [...state, newplane]
+    }
+    return state;
+}
 
 /** TODO: Create store */
-
+const planes = createStore(
+    combineReducers({
+      airlines
+    })
+  )
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={planes}>
     <React.StrictMode>
         <App />
     </React.StrictMode>
+    </Provider>
 );
